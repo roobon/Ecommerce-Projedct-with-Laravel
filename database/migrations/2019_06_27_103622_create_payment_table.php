0a1fd88450dfd6b42300c2a5fbed528715c9992a
Iftekhar Shakkhor <52200505+ishakkhor@users.noreply.github.com>
2019-06-27 16:49:26 +0600

    Create 2019_06_27_103622_create_payment_table.php
    payment table made by ishakkhor

---
+++ database/migrations/2019_06_27_103622_create_payment_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaymentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment', function (Blueprint $table) {
+            $table->string('payment_id');
+            $table->integer('payment_method');
+            $table->integer('payment_status');
+            $table->timestamp('payment_date_time');
+            
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment');
+    }
+}
