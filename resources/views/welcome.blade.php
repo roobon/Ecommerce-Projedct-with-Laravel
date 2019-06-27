03205bdc204f8587b3f6de7d75d82c1dc05bb4f9
Md. Saiful Islam <dupl.saifulmolin@gmail.com>
2019-06-27 15:45:27 +0600

    view
    Hello Milon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -89,6 +89,9 @@
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
+                <div>
+                    <h1>Hello Milon</h1>
+                </div>
             </div>
         </div>
     </body>
