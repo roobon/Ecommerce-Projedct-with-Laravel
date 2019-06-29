cb6bd75f3ae01334ac16b72c6bac550861bae5e7
Syed Ziaul Habib <roobon@gmail.com>
2019-06-29 15:04:22 +0600

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -163,7 +163,7 @@
 									<button type="button" class="btn btn-default get">Get it now</button>
 								</div>
 								<div class="col-sm-6">
-									<img src="images/home/girl1.jpg" class="girl img-responsive" alt="" />
+									<img src="https://i.imgur.com/vl9FJ6u.jpg" class="girl img-responsive" alt="" />
 									<img src="images/home/pricing.png"  class="pricing" alt="" />
 								</div>
 							</div>
