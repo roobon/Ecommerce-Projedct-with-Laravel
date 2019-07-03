9ff60353ac4504790c1dadcb1bbe24caf215057f
Syed Ziaul Habib <roobon@gmail.com>
2019-07-03 16:05:56 +0600

    Update default.blade.php

--- resources/views/admin/layouts/default.blade.php
+++ resources/views/admin/layouts/default.blade.php
@@ -34,8 +34,12 @@
 	<![endif]-->
 		
 	<!-- start: Favicon -->
-	<link rel="shortcut icon" href="img/favicon.ico">
-	<!-- end: Favicon -->	
+	<link rel="shortcut icon" href="{{asset('backend/img/favicon.ico')}}">
+	<!-- end: Favicon -->
+	
+		
+		
+		
 </head>
 
 <body>
@@ -47,7 +51,7 @@
 		<div class="row-fluid">
 				
 			<!-- start: Main Menu -->
-			@include('admin.layouts.leftbar')
+	@include('admin.layouts.leftbar')		
 			<!-- end: Main Menu -->
 			
 			<noscript>
@@ -58,9 +62,24 @@
 			</noscript>
 			
 			<!-- start: Content -->
-		@yield('admincontent')
+		<div id="content" class="span10">
+			
+			
+			<ul class="breadcrumb">
+				<li>
+					<i class="icon-home"></i>
+					<a href="index.html">Home</a> 
+					<i class="icon-angle-right"></i>
+				</li>
+				<li><a href="#">Dashboard</a></li>
+			</ul>
+
+						
+		@yield('admincontent')	
+
+		</div><!--/.fluid-container-->
 	
-		<!-- end: Content -->
+			<!-- end: Content -->
 		</div><!--/#content.span10-->
 		</div><!--/fluid-row-->
 		
@@ -85,7 +104,7 @@
 	<!-- start: JavaScript-->
 
 		<script src="{{asset('backend/js/jquery-1.9.1.min.js')}}"></script>
-	<script src="{{asset('backend/js/jquery-migrate-1.0.0.min.js')}}"></script>
+		<script src="{{asset('backend/js/jquery-migrate-1.0.0.min.js')}}"></script>
 	
 		<script src="{{asset('backend/js/jquery-ui-1.10.0.custom.min.js')}}"></script>
 	
@@ -95,49 +114,49 @@
 	
 		<script src="{{asset('backend/js/bootstrap.min.js')}}"></script>
 	
-		<script src="js/jquery.cookie.js"></script>
+		<script src="{{asset('backend/js/jquery.cookie.js')}}"></script>
 	
-		<script src='js/fullcalendar.min.js'></script>
+		<script src='{{asset('backend/js/fullcalendar.min.js')}}'></script>
 	
-		<script src='js/jquery.dataTables.min.js'></script>
+		<script src='{{asset('backend/js/jquery.dataTables.min.js')}}'></script>
 
-		<script src="js/excanvas.js"></script>
-	<script src="js/jquery.flot.js"></script>
-	<script src="js/jquery.flot.pie.js"></script>
-	<script src="js/jquery.flot.stack.js"></script>
-	<script src="js/jquery.flot.resize.min.js"></script>
+		<script src="{{asset('backend/js/excanvas.js')}}"></script>
+		<script src="{{asset('backend/js/jquery.flot.js')}}"></script>
+		<script src="{{asset('backend/js/jquery.flot.pie.js')}}"></script>
+		<script src="{{asset('backend/js/jquery.flot.stack.js')}}"></script>
+		<script src="{{asset('backend/js/jquery.flot.resize.min.js')}}"></script>
 	
-		<script src="js/jquery.chosen.min.js"></script>
+		<script src="{{asset('backend/js/jquery.chosen.min.js')}}"></script>
 	
-		<script src="js/jquery.uniform.min.js"></script>
+		<script src="{{asset('backend/js/jquery.uniform.min.js')}}"></script>
 		
-		<script src="js/jquery.cleditor.min.js"></script>
+		<script src="{{asset('backend/js/jquery.cleditor.min.js')}}"></script>
 	
-		<script src="js/jquery.noty.js"></script>
+		<script src="{{asset('backend/js/jquery.noty.js')}}"></script>
 	
-		<script src="js/jquery.elfinder.min.js"></script>
+		<script src="{{asset('backend/js/jquery.elfinder.min.js')}}"></script>
 	
-		<script src="js/jquery.raty.min.js"></script>
+		<script src="{{asset('backend/js/jquery.raty.min.js')}}"></script>
 	
-		<script src="js/jquery.iphone.toggle.js"></script>
+		<script src="{{asset('backend/js/jquery.iphone.toggle.js')}}"></script>
 	
-		<script src="js/jquery.uploadify-3.1.min.js"></script>
+		<script src="{{asset('backend/js/jquery.uploadify-3.1.min.js')}}"></script>
 	
-		<script src="js/jquery.gritter.min.js"></script>
+		<script src="{{asset('backend/js/jquery.gritter.min.js')}}"></script>
 	
-		<script src="js/jquery.imagesloaded.js"></script>
+		<script src="{{asset('backend/js/jquery.imagesloaded.js')}}"></script>
 	
-		<script src="js/jquery.masonry.min.js"></script>
+		<script src="{{asset('backend/js/jquery.masonry.min.js')}}"></script>
 	
-		<script src="js/jquery.knob.modified.js"></script>
+		<script src="{{asset('backend/js/jquery.knob.modified.js')}}"></script>
 	
-		<script src="js/jquery.sparkline.min.js"></script>
+		<script src="{{asset('backend/js/jquery.sparkline.min.js')}}"></script>
 	
-		<script src="js/counter.js"></script>
+		<script src="{{asset('backend/js/counter.js')}}"></script>
 	
-		<script src="js/retina.js"></script>
+		<script src="{{asset('backend/js/retina.js')}}"></script>
 
-		<script src="js/custom.js"></script>
+		<script src="{{asset('backend/js/custom.js')}}"></script>
 	<!-- end: JavaScript-->
 	
 </body>
