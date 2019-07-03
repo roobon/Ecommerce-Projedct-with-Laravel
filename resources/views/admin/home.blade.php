480970970f041744ec44fb3d8bd7fcf28d6d28b7
AhmadTanvir <atasiddiq@gmail.com>
2019-07-03 06:51:13 -0700

    Update home.blade.php

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,35 +3,35 @@
 @section('admincontent')
 	<div class="row-fluid">	
 
-				<a class="quick-button metro yellow span1" style="width: 90px; height: 122px">
+				<a class="quick-button metro yellow span1" style="width: 112px; height: 122px">
 					<i class="icon-group"></i>
 					<p>Users 237</p>
 				</a>
-				<a class="quick-button metro red span1" style="width: 110px; height: 122px">
+				<a class="quick-button metro red span1" style="width: 112px; height: 122px">
 					<i class="icon-comments-alt"></i>
 					<p>Comments 46</p>
 				</a>
-				<a class="quick-button metro blue span1" style="width: 110px; height: 122px">
+				<a class="quick-button metro blue span1" style="width: 112px; height: 122px">
 					<i class="icon-shopping-cart"></i>
 					<p>Orders 13</p>
 				</a>
-				<a class="quick-button metro green span1" style="width: 110px; height: 122px">
+				<a class="quick-button metro green span1" style="width: 112px; height: 122px">
 					<i class="icon-barcode"></i>
 					<p>Products</p>
 				</a>
-				<a class="quick-button metro pink span1" style="width: 110px; height: 122px">
+				<a class="quick-button metro pink span1" style="width: 112px; height: 122px">
 					<i class="icon-envelope"></i>
 					<p>Messages 88</p>
 				</a>
-				<a class="quick-button metro black span1" style="width: 110px; height: 122px">
+				<a class="quick-button metro black span1" style="width: 112px; height: 122px">
 					<i class="icon-calendar"></i>
 					<p>Calendar</p>
 				</a>
-				<a class="quick-button metro yellow span1" style="width: 110px; height: 122px">
+				<a class="quick-button metro blue span1" style="width: 112px; height: 122px">
 					<i class="icon-group"></i>
 					<p>Customers 237</p>
 				</a>
-				<a class="quick-button metro yellow span1" style="width: 110px; height: 122px">
+				<a class="quick-button metro green span1" style="width: 112px; height: 122px">
 					<i class="icon-group"></i>
 					<p>Menufecturer 50</p>
 				</a>
