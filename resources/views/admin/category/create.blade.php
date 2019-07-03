78f9e794718bc466b79fe34eb990c07eb53036bf
Kabir <khyrulkabir01@gmail.com>
2019-07-03 18:01:43 +0600

    Update create.blade.php

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -7,22 +7,22 @@
 						<div class="box-icon">
 							<a href="#" class="btn-setting"><i class="halflings-icon wrench"></i></a>
 							<a href="#" class="btn-minimize"><i class="halflings-icon chevron-up"></i></a>
-							<a href="#" class="btn-close"><i class="halflings-icon remove"  ></i></a>
+							<a href="#" class="btn-close"><i class="halflings-icon remove"></i></a>
 						</div>
 					</div>
 					<div class="box-content">
 						<form class="form-horizontal">
 							<fieldset>
 						
-							  <div class="control-group success">
-								<label class="control-label" for="input">category name</label>
+							  <div class="control-group ">
+								<label style="color:black" class="control-label" for="prependedInput">category name</label>
 								<div class="controls">
-								  <input name="category_name" type="text" id="input">
+								  <input name="category_name" type="text" id="inputSuccess">
 								  <!-- <span class="help-inline">Woohoo!</span> -->
 								</div>
 							  </div>
-                              <div class="control-group success">
-								<label class="control-label" for="input">category description</label>
+                              <div class="control-group ">
+								<label style="color: black" class="control-label" for="input">category description</label>
 								<div class="controls">
                                 
                               
@@ -36,10 +36,10 @@
 								</div>
 							  </div>
                       
-							  <div class="control-group">
-								<label class="control-label" for="selectError3">publication status</label>
+							  <div class="control-group ">
+								<label style="color: black" class="control-label" for="selectError3">publication status</label>
 								<div class="controls">
-								  <select id="selectError3" name="publication_status">
+								  <select id="selectError3" name="publication_status" >
 									<option value="" selected hidden>select one</option>
 									<option value="1">published</option>
 									<option value="0">draft</option>
