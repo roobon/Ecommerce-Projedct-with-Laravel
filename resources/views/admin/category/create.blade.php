5f32f29ddfea186a81442d5d1db6784ac40414bd
nasimasheikh <52200293+nasimasheikh@users.noreply.github.com>
2019-07-03 17:35:57 +0600

    Update create.blade.php

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -15,14 +15,14 @@
 							<fieldset>
 						
 							  <div class="control-group success">
-								<label class="control-label" for="inputSuccess">category name</label>
+								<label class="control-label" for="input">category name</label>
 								<div class="controls">
-								  <input name="category_name" type="text" id="inputSuccess">
+								  <input name="category_name" type="text" id="input">
 								  <!-- <span class="help-inline">Woohoo!</span> -->
 								</div>
 							  </div>
                               <div class="control-group success">
-								<label class="control-label" for="inputSuccess">category description</label>
+								<label class="control-label" for="input">category description</label>
 								<div class="controls">
                                 
                               
