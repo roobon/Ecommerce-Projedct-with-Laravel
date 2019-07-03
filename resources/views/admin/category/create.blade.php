d97f4a04d2233557745be6378d6ae495198766dd
nasimasheikh <52200293+nasimasheikh@users.noreply.github.com>
2019-07-03 17:29:39 +0600

    Update create.blade.php

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -1,13 +1,13 @@
 @extends('admin.layouts.default')
 @section('admincontent')
 <div class="row-fluid sortable ui-sortable">
-				<div class="box span12">
+				<div class="box blue span12">
 					<div class="box-header" data-original-title="">
-						<h2><i class="halflings-icon edit"></i><span class="break" class="btn btn-warning" ></span>create category</h2>
+						<h2><i class="halflings-icon edit"></i><span class="break"></span>create category</h2>
 						<div class="box-icon">
 							<a href="#" class="btn-setting"><i class="halflings-icon wrench"></i></a>
 							<a href="#" class="btn-minimize"><i class="halflings-icon chevron-up"></i></a>
-							<a href="#" class="btn-close"><i class="halflings-icon remove"></i></a>
+							<a href="#" class="btn-close"><i class="halflings-icon remove"  ></i></a>
 						</div>
 					</div>
 					<div class="box-content">
