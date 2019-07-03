0b803f976e0ed6ab76fed92b357293ca248ccebf
Sharfuzzamaan <47491930+Sharfuzzamaan@users.noreply.github.com>
2019-07-03 18:34:20 +0600

    Update index.blade.php

--- resources/views/admin/customer/index.blade.php
+++ resources/views/admin/customer/index.blade.php
@@ -1,25 +1,40 @@
 <!-- index.blade.php -->
 @extends('admin.layouts.default')
 @section('admincontent')
-  <div class="container">
-    <table class="table table-striped">
-    <thead>
-      <tr>
-        <th>Customer Name</th>
-        <th>Customer E-mail</th>
-        <th>Customer Phone</th>
-      </tr>
-    </thead>
-    <tbody>
-      @foreach($allcustomer as $customer)
-      <tr>
-        <td>{{$customer['first_name']}} {{$customer['last_name']}}</td>
-        <td>{{$customer['email_address']}}</td>
-        <td>{{$customer['telephone']}}</td>
-      </tr>
+  
 
-      @endforeach
-    </tbody>
-  </table>
-  </div>
+<div class="box-content">
+            <table class="table table-striped table-bordered bootstrap-datatable datatable">
+              <thead>
+                <tr>
+                  <th>Customer Username</th>
+                  <th>Date registered</th>
+                  <th>Customer E-mail</th>
+                  <th>Cutomer Phone</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>   
+              <tbody>
+                 @foreach($allcustomer as $customer)
+              <tr>
+                <td>{{$customer['first_name']}} {{$customer['last_name']}}</td>
+                <td class="center">{{$customer['created_at']}}</td>
+                <td class="center">{{$customer['email_address']}}</td>
+                <td class="center">{{$customer['telephone']}}</td>
+                <td class="center">
+                  <a class="btn btn-success" href="#">
+                    <i class="halflings-icon white zoom-in"></i>  
+                  </a>
+                  <a class="btn btn-info" href="#">
+                    <i class="halflings-icon white edit"></i>  
+                  </a>
+                  <a class="btn btn-danger" href="#">
+                    <i class="halflings-icon white trash"></i> 
+                  </a>
+                </td>
+              </tr>
+               @endforeach
+              </tbody>
+            </table>            
+          </div>
 @stop
