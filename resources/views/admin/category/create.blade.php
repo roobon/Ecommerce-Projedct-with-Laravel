5605f9eff028cc7e00475882e2a332a905ca3568
nasimasheikh <52200293+nasimasheikh@users.noreply.github.com>
2019-07-03 16:43:10 +0600

    Update create.blade.php

---
+++ resources/views/admin/category/create.blade.php
@@ -0,0 +1,64 @@
+@extends('admin.layouts.default')
+@section('admincontent')
+<div class="row-fluid sortable ui-sortable">
+				<div class="box span12">
+					<div class="box-header" data-original-title="">
+						<h2><i class="halflings-icon edit"></i><span class="break"></span>create category</h2>
+						<div class="box-icon">
+							<a href="#" class="btn-setting"><i class="halflings-icon wrench"></i></a>
+							<a href="#" class="btn-minimize"><i class="halflings-icon chevron-up"></i></a>
+							<a href="#" class="btn-close"><i class="halflings-icon remove"></i></a>
+						</div>
+					</div>
+					<div class="box-content">
+						<form class="form-horizontal">
+							<fieldset>
+						
+							  <div class="control-group success">
+								<label class="control-label" for="inputSuccess">category name</label>
+								<div class="controls">
+								  <input name="category_name" type="text" id="inputSuccess">
+								  <!-- <span class="help-inline">Woohoo!</span> -->
+								</div>
+							  </div>
+                              <div class="control-group success">
+								<label class="control-label" for="inputSuccess">category description</label>
+								<div class="controls">
+                                
+                              
+                                    <div class="col-sm-10">
+                                        <textarea name="category_description" id="textarea" class="form-control" rows="3" required="required"></textarea>
+                                    </div>
+                                
+                                
+								  
+								  <!-- <span class="help-inline">Woohoo!</span> -->
+								</div>
+							  </div>
+                      
+							  <div class="control-group">
+								<label class="control-label" for="selectError3">publication status</label>
+								<div class="controls">
+								  <select id="selectError3" name="publication_status">
+									<option value="" selected hidden>select one</option>
+									<option value="1">published</option>
+									<option value="0">draft</option>
+									
+								  </select>
+								</div>
+							  </div>
+
+							  <div class="form-actions">
+								<button type="submit" class="btn btn-primary">Save changes</button>
+								<button class="btn">Cancel</button>
+							  </div>
+							</fieldset>
+						  </form>
+					
+					</div>
+				</div><!--/span-->
+			
+			</div>
+
+
+@stop
