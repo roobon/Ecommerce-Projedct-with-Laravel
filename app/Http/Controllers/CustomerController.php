0bdb8fe34dc267051d359ad00d8dba620dde578d
Sharfuzzamaan <47491930+Sharfuzzamaan@users.noreply.github.com>
2019-07-04 18:31:16 +0600

    Update CustomerController.php

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -92,9 +92,6 @@ class CustomerController extends Controller
      */
     public function destroy($id)
     {
-        $customer = Customer::find($id);
-        $customer->delete();
-
-        return redirect('admin/customer');
+        //
     }
 }
