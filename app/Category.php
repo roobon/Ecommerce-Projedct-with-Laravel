5cbcc2f732a20e6541cec52ba866d17653f16882
Iftekhar Shakkhor <52200505+ishakkhor@users.noreply.github.com>
2019-07-04 17:39:16 +0600

    category

--- app/Category.php
+++ app/Category.php
@@ -9,7 +9,5 @@ class Category extends Model
     // protect table name
 	protected $table='category'; 
     //  catagory store many product
-    public function product() {
-     return $this -> hasMany(Product::class);
-}
+   
 }
