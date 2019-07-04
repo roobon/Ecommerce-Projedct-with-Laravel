1741ab1221c46dbedab50c36f283a9f92d529d3b
Kabir <khyrulkabir01@gmail.com>
2019-07-04 19:10:13 +0600

    Update edit.blade.php

--- resources/views/admin/product/edit.blade.php
+++ resources/views/admin/product/edit.blade.php
@@ -1,9 +1,19 @@
 @extends('admin.layouts.default')
 @section('admincontent')
+
 <div class="row-fluid sortable ui-sortable">
 				<div class="box blue span12">
+					@if ($errors->any())
+					<div class="alert alert-danger">
+						<ul>
+							@foreach ($errors->all() as $error)
+							<li>{{ $error }}</li>
+							@endforeach
+						</ul>
+					</div>
+					@endif
 					<div class="box-header" data-original-title="">
-						<h2><i class="halflings-icon edit"></i><span class="break"></span>Add category</h2>
+						<h2><i class="halflings-icon edit"></i><span class="break"></span>Update category</h2>
 						<div class="box-icon">
 							<a href="#" class="btn-setting"><i class="halflings-icon wrench"></i></a>
 							<a href="#" class="btn-minimize"><i class="halflings-icon chevron-up"></i></a>
@@ -11,28 +21,36 @@
 						</div>
 					</div>
 					<div class="box-content">
-						<form class="form-horizontal">
+						<form class="form-horizontal" action="{{action('ProductController@update', $id)}}" method="post">
                             <fieldset>
+                            	<input name="_token" type="hidden" value="{{ csrf_token() }}"/>
                             <div class="control-group ">
 								<label style="color: black" class="control-label" for="selectError4">Select Category</label>
 								<div class="controls">
 								  <select id="selectError3" name="publication_status" >
 									<option value="" selected hidden>select one</option>
-                                    @foreach($allProduct as $product)
-									<option value="{{$product->category_id}}">{{$product->category->category_name}}</option>
-                                    @endforeach
+                                    
+									<option value="{{$Cat->category_id}}" selected="">{{$Cat->category_name}}</option>
+                                   
 								  </select>
 								</div>
 							  </div>
                               <div class="control-group ">
 								<label style="color: black" class="control-label" for="selectError3">Select manufacturer</label>
 								<div class="controls">
-								  <select id="selectError3" name="publication_status" >
-									<option value="" selected hidden>select one</option>
-                                    @foreach($allProduct as $product)
-									<option value="{{$product->manufacturer_id}}">{{$product->manufacturer->manufacturer_name}}</option>
-                                    @endforeach
-								  </select>
+
+									<select id="selectError3" name="manufacturer_id">
+										<option value="" selected hidden>select one</option>
+										@foreach($allManufacturer as )
+										<option value="{{ $manu->id }}"
+
+										@if ($manu->id == old('manufacturer_id', $model->option))
+										selected="selected"
+										@endif
+										>{{ $manu->manufacturer_name }}</option>
+										@endforeach
+									</select>
+								  
 								</div>
 							  </div>
                             </fieldset>
@@ -42,14 +60,14 @@
 							  <div class="control-group ">
 								<label style="color:black" class="control-label" for="prependedInput">product name</label>
 								<div class="controls">
-								  <input name="product_name" type="text" id="inputSuccess">
+								  <input name="product_name" type="text" id="inputSuccess" value="{{$product->product_name}}">
 								  <!-- <span class="help-inline">Woohoo!</span> -->
 								</div>
 							  </div>
 							  <div class="control-group ">
 								<label style="color:black" class="control-label" for="prependedInput">product_short_description</label>
 								<div class="controls">
-								  <input name="product_short_description" type="text" id="inputSuccess">
+								  <input name="product_short_description" type="text" id="inputSuccess" value="{{$product->product_short_description}}">
 								  <!-- <span class="help-inline">Woohoo!</span> -->
 								</div>
 							  </div>
@@ -58,7 +76,7 @@
 								<div class="controls">                              
                               
                                     <div class="col-sm-10">
-                                        <textarea name="product_long_description" id="textarea" class="form-control" rows="3" required="required"></textarea>
+                                        <textarea name="product_long_description" id="textarea" class="form-control" rows="3" required="required">{{$product->product_long_description}}</textarea>
                                     </div>                            
                                 
 								  
@@ -66,9 +84,9 @@
 								</div>
                               </div>
                               <div class="control-group ">
-								<label style="color:black" class="control-label" for="prependedInput">product_price</label>
+								<label style="color:black" class="control-label" for="prependedInput">product_long_description</label>
 								<div class="controls">
-								  <input name="product_price" type="text" id="inputSuccess">
+								  <input name="product_price" type="text" id="inputSuccess" value="{{$product->product_long_description}}">
 								  <!-- <span class="help-inline">Woohoo!</span> -->
 								</div>
 							  </div>
@@ -83,14 +101,14 @@
                               <div class="control-group ">
 								<label style="color:black" class="control-label" for="prependedInput">product_size</label>
 								<div class="controls">
-								  <input name="product_size" type="text" id="inputSuccess">
+								  <input name="product_size" type="text" id="inputSuccess" value="{{$product->product_size}}">
 								  <!-- <span class="help-inline">Woohoo!</span> -->
 								</div>
 							  </div>
                               <div class="control-group ">
 								<label style="color:black" class="control-label" for="prependedInput">product_color</label>
 								<div class="controls">
-								  <input name="product_color" type="text" id="inputSuccess">
+								  <input name="product_color" type="text" id="inputSuccess" value="{{$product->product_color}}">
 								  <!-- <span class="help-inline">Woohoo!</span> -->
 								</div>
 							  </div>
