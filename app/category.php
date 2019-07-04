fd655fc102be4817452c1d320073b4e859d9c072
reza1-web <52201009+reza1-web@users.noreply.github.com>
2019-07-04 16:57:56 +0600

    Update category.php

--- app/category.php
+++ app/category.php
@@ -4,7 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class category extends Model
+class Category extends Model
 {
-    //
+    // protect table name
+	protected $table='category'; 
+    //  catagory store many product
+    public function product() {
+     return $this -> hasMany(Product::class);
+}
 }
