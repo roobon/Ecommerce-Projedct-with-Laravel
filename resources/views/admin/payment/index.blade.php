ddb592a7ffb182892c16de3fb22499ae49bada0d
Nazmulon <nazmulon0@gmail.com>
2019-07-04 19:22:49 +0600

    Update index.blade.php

--- resources/views/admin/payment/index.blade.php
+++ resources/views/admin/payment/index.blade.php
@@ -1,22 +1,30 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Payment Status</title>
-</head>
-<body>
+@extends('admin.layouts.default)
+@section('admincontent')
     <table>
         <tr>
             <th>ID</th>
             <th>Payment Methed</th>
             <th>Payment Status</th>
+            <th>Action</th>
         </tr>
         <tr>
-            <td>{{}}</td>
+            @foreach($payments as $payment)
+            <td>{{$payment->payment_id}}</td>
+            <td>{{$payment->payment_method}}</td>
+            <td>{{$payment->payment_status}}</td>
+            <td>{{$payment->payment_date_time}}</td>
+                <td>
+                    <a href="" class="btn btn-success">
+                        <i class="halflings-icon white zoom-in"></i>
+                    </a>
+                    <a href="" class="btn btn-info">
+                        <i class="halflings-icon white edit"></i>
+                    </a>
+                    <a href="" class="btn btn-danger">
+                        <i class="halflings-icon white trash"></i>
+                    </a>
+                </td>
+            @endforeach
         </tr>
     </table>
-</body>
-</html>
+@stop
