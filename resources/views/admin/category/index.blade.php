d14d7646843f189e8c96b0d7d92f445f9f5bdab1
Habibur092 <52200164+Habibur092@users.noreply.github.com>
2019-07-06 16:12:17 +0600

    Update index.blade.php

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -5,9 +5,7 @@
 					<div class="box-header" data-original-title>
 						<h2><i class="halflings-icon user"></i><span class="break"></span>Categories</h2>
 						<div class="box-icon">
-							<a href="#" class="btn-setting"><i class="halflings-icon wrench"></i></a>
 							<a href="#" class="btn-minimize"><i class="halflings-icon chevron-up"></i></a>
-							<a href="#" class="btn-close"><i class="halflings-icon remove"></i></a>
 						</div>
 					</div>
 					<div class="box-content">
