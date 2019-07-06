2c57d6584b9789e24b3cc1bacee6d3930901df3f
Kabir <44431386+kabirkhyrul@users.noreply.github.com>
2019-07-06 14:34:11 +0600

    Update Product.php

--- app/Product.php
+++ app/Product.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
    protected $table='product';
+   // make field fillable
+protected $fillable = ['category_id','manufacturer_id','product_name','product_short_description','product_long_description','product_price','product_image','product_size','product_color','publication_status'];
    
 //    category have this product
 
