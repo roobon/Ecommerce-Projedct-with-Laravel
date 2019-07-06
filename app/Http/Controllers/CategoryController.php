002ecf5679068245447145c0621247a2c3fffbd1
Habibur092 <52200164+Habibur092@users.noreply.github.com>
2019-07-06 16:02:20 +0600

    Update CategoryController.php

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Category;
 
 class CategoryController extends Controller
 {
@@ -14,8 +15,8 @@ class CategoryController extends Controller
      */
     public function index()
     {
-
-        return view('admin.category.index');
+        $categorys = Category::all()->toArray();
+        return view('admin.category.index', compact('categorys'));
     }
 
     /**
