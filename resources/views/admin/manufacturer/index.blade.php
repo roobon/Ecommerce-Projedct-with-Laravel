3d36ad8e9ae11fcac22516bc5c1a28d79ab46bf1
Kabir <khyrulkabir01@gmail.com>
2019-07-07 17:59:18 +0600

    Update index.blade.php

--- resources/views/admin/manufacturer/index.blade.php
+++ resources/views/admin/manufacturer/index.blade.php
@@ -15,20 +15,21 @@
                 </tr>
               </thead>   
               <tbody>
-                @foreach($manufacturer as $manufacturer)
+
+                @foreach($manufacturer as $manufact)
                 
               <tr>
                 
-                <td class="center">{{$manufacturer->manufacturer_name}}</td>
-                <td class="center">{{$manufacturer->description}}</td>
+                <td class="center">{{$manufact->manufacturer_name}}</td>
+                <td class="center">{{$manufact->description}}</td>
                 
                 <td class="center">
                   <a class="btn btn-success" href="#">
                     <i class="halflings-icon white zoom-in"></i>  
                   </a>
-                  <a href="{{route('product.edit',$product->id)}}" class="btn btn-warning"><i class="halflings-icon white edit"></i> </a>
+                  <a href="{{route('manufacturer.edit',$manufact->id)}}" class="btn btn-warning"><i class="halflings-icon white edit"></i> </a>
                   
-                  <form action="{{route('product.destroy',$product->id)}}" method="post">
+                  <form action="{{route('manufacturer.destroy',$manufact->id)}}" method="post">
                   {{csrf_field()}}
                 <input name="_method" type="hidden" value="DELETE">
                  <button class="btn btn-danger" type="submit">Delete</button>
