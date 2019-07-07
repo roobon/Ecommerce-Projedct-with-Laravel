62a6cbe28d2af5ece007b618c74a7ac84feaff53
AhmadTanvir <atasiddiq@gmail.com>
2019-07-07 06:01:44 -0700

    Update Shippings.php

--- app/Shippings.php
+++ app/Shippings.php
@@ -9,4 +9,6 @@ class Shippings extends Model
     protected $table='shipping';
 
     protected $primaryKey = 'shipping_id';
+
+    protected $fillable = ['shipping_first_name','shipping_last_name','shipping_address','shipping_telephone','shipping_email'];
 }
