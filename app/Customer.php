9db88e50d971c37bc7a2336bd77997f1e0ce1b54
Syed Ziaul Habib <roobon@gmail.com>
2019-07-08 16:11:53 +0600

    Update Customer.php

--- app/Customer.php
+++ app/Customer.php
@@ -3,9 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Order;
 
 class Customer extends Model
 {
    protected $table='customer'; 
    protected $primaryKey = 'customer_id';
+
+   public function order(){
+    	$this->hasMany(Order::class);
+    }
 }
