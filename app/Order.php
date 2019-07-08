6c5f97ad837572832ff0a37ba1f4bc1d91173ac4
Syed Ziaul Habib <roobon@gmail.com>
2019-07-08 16:12:00 +0600

    Update Order.php

--- app/Order.php
+++ app/Order.php
@@ -3,9 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Customer;
+use App\Shippings;
+use App\Payment;
 
 class Order extends Model
 {
     protected $table = 'orders';
     protected $primaryKey = 'order_id';
+
+    public function customer(){
+    	$this->belongsTo(Customer::class);
+    }
+
+    public function shippings(){
+    	$this->hasOne(Shippings::class);
+    }
+
+    public function payment(){
+    	$this->hasOne(Payment::class);
+    }
 }
