cf8e757cacd5771f5cffeda6f045880f580c57bf
Syed Ziaul Habib <roobon@gmail.com>
2019-07-08 16:12:03 +0600

    Update Payment.php

--- app/Payment.php
+++ app/Payment.php
@@ -3,10 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Order;
 
 class Payment extends Model
 {
     protected $table= 'payment';
     protected $primaryKey = 'payment_id';
     protected $fillable = ['payment_method', 'payment_status'];
+
+     public function order(){
+    	$this->hasOne(Order::class);
+    }
 }
